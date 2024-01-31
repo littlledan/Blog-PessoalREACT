bbdeed9d4584d2d1bdf91af1339fd5bfd16aaa3d
littlledan <daniel799.dl@gmail.com>
2024-01-31 08:02:06 -0300

    Adicionando tema aos alertas

--- src/util/toastAlerta.ts
+++ src/util/toastAlerta.ts
@@ -1,56 +1,60 @@
-import { toast } from 'react-toastify';
+import { Bounce, toast } from 'react-toastify';
 
 export function toastAlerta(mensagem: string, tipo: string) {
   switch (tipo) {
     case 'sucesso':
       toast.success(mensagem, {
-        position: 'top-right',
-        autoClose: 2000,
+        position: "top-right",
+        autoClose: 5000,
         hideProgressBar: false,
         closeOnClick: true,
-        pauseOnHover: false,
-        draggable: false,
-        theme: 'colored',
+        pauseOnHover: true,
+        draggable: true,
         progress: undefined,
+        theme: "dark",
+        transition: Bounce,
       });
       break;
 
     case 'info':
       toast.info(mensagem, {
-        position: 'top-right',
-        autoClose: 2000,
+        position: "top-right",
+        autoClose: 5000,
         hideProgressBar: false,
         closeOnClick: true,
-        pauseOnHover: false,
-        draggable: false,
-        theme: 'colored',
+        pauseOnHover: true,
+        draggable: true,
         progress: undefined,
+        theme: "dark",
+        transition: Bounce,
       });
       break;
 
     case 'erro':
       toast.error(mensagem, {
-        position: 'top-right',
-        autoClose: 2000,
+        position: "top-right",
+        autoClose: 5000,
         hideProgressBar: false,
         closeOnClick: true,
-        pauseOnHover: false,
-        draggable: false,
-        theme: 'colored',
+        pauseOnHover: true,
+        draggable: true,
         progress: undefined,
+        theme: "dark",
+        transition: Bounce,
       });
       break;
 
     default:
       toast.info(mensagem, {
-        position: 'top-right',
-        autoClose: 2000,
+        position: "top-right",
+        autoClose: 5000,
         hideProgressBar: false,
         closeOnClick: true,
-        pauseOnHover: false,
-        draggable: false,
-        theme: 'colored',
+        pauseOnHover: true,
+        draggable: true,
         progress: undefined,
+        theme: "dark",
+        transition: Bounce,
       });
       break;
   }
